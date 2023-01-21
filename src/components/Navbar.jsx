c751595cf99c7532930ce919af1781821a2499f5
Jackson <z5397791@ad.unsw.edu.au>
2023-01-21 21:21:46 +1100

    Add mobile menu style in navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,23 +13,7 @@ function Navbar() {
       </div>
 
       {/* menu */}
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      {/* Mobile menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -37,8 +21,25 @@ function Navbar() {
         <li>Contact</li>
       </ul>
 
+      {/* Hamburger */}
+      <div className="md:hidden z-10">
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className="hidden absolute top-0 left-0 w-full h-screen 
+        bg-[#0a192f] flex flex-col justify-center items-center"
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
       {/* Social icons */}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   );
 }
