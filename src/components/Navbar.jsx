1e70cec961b5c6e009d6bc2640d812b23e45e12e
Jackson <z5397791@ad.unsw.edu.au>
2023-01-21 20:54:10 +1100

    Add navigation bar component to home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div
+      className="fixed w-full h-[80px] flex justify-between 
+      items-center px-4 bg-[#0a192f] text-gray-300"
+    >
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
