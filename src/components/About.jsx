780b04d1e3bc052d2f877cce943ea35744e1f38b
Jackson <z5397791@ad.unsw.edu.au>
2023-01-22 20:02:03 +1100

    Add title to container in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
         {/* Container */}
-        <div>
-          <div>
-            <p>About</p>
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <></>
+          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+            <div>
+              <p>Hi, I'm Jackson, nice to meet you. Please take a look around.</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
