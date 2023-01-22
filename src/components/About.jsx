3246849a62c9a05239491ec1b749db77567df847
Jackson <z5397791@ad.unsw.edu.au>
2023-01-22 20:01:16 +1100

    Add about section to components

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        {/* Container */}
+        <div>
+          <div>
+            <p>About</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
