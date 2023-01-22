b9f21c07887af8f1c692461e7a46af35c9b3c9af
Jackson <z5397791@ad.unsw.edu.au>
2023-01-22 19:48:20 +1100

    Add custom tailwind button to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,10 +18,34 @@ function Home() {
           building responsive multiplatform web applications.
         </p>
         <div>
-          <button>
-            View Work <HiArrowNarrowRight />
-          </button>
-          
+          {/* <button
+            className="text-white group border-2 px-6 py-3 my-2 flex items-center
+                  hover:bg-pink-600 hover:border-pink-600"
+          >
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button> */}
+          <a
+            href="#_"
+            class="relative inline-flex items-center justify-start inline-block
+            px-6 py-4 overflow-hidden group"
+          >
+            <span
+              class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2
+             absolute left-0 top-0 bg-white opacity-[3%]"
+            ></span>
+            <span
+              class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all 
+            duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24
+             bg-pink-600 opacity-100 group-hover:-translate-x-8"
+            ></span>
+            <span class="relative w-full text-left text-white transition-colors duration-100 ease-in-out">
+              View Work
+            </span>
+            <span class="absolute inset-0 border-2 border-white hover:border-pink-600 duration-200 "></span>
+          </a>
         </div>
       </div>
     </div>
