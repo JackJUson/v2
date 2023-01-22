b9a9a9fff77644daa413cb00c28b7b1d2a1c98f4
Jackson <z5397791@ad.unsw.edu.au>
2023-01-22 21:13:47 +1100

    Add skill component for each image in skills component

---
+++ src/components/Skill.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Skill({ image, title }) {
+  return (
+    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+      <img className="w-20 my-4 mx-auto" src={image} alt={`${title} Icon`}/>
+      <p className="my-4">{title}</p>
+    </div>
+  );
+}
+
+export default Skill;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,21 +8,27 @@ import Node from '../assets/node-js.png';
 import FireBase from '../assets/firebase.png';
 import Tailwind from '../assets/tailwind.png';
 import Electron from '../assets/electron.png';
+import Skill from './Skill';
 
 function Skills() {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-grey-300'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p>Experience</p>
-          <p>Thse are the technologies I've worked with!</p>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+          <p className='py-4'>These are the technologies I've worked with!</p>
         </div>
 
-        <div>
-          <div>
-            <img src={HTML} alt="HTML Icon" />
-          </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <Skill image={HTML} title={'HTML'} />
+          <Skill image={CSS} title={'CSS'} />
+          <Skill image={JavaScript} title={'JavaScript'} />
+          <Skill image={ReactImg} title={'React'} />
+          <Skill image={Electron} title={'Electron'} />
+          <Skill image={FireBase} title={'FireBase'} />
+          <Skill image={Tailwind} title={'Tailwind'} />
+          <Skill image={Node} title={'Node'} />
         </div>
       </div>
     </div>
