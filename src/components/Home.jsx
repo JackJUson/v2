7c9b3725416fd26d3321399193a001aaac52ea1b
Jackson <z5397791@ad.unsw.edu.au>
2023-01-22 18:50:12 +1100

    Add typography style to home container div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,22 @@ function Home() {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1>Jackson Jung</h1>
-        <h2>I'm a Frontend Web Developer.</h2>
-        <p>
-          I have a strong passion for software engineering with 
-          experience building responsive multiplatform web applications.
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Jackson Jung
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Frontend Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I have a strong passion for software engineering with experience
+          building responsive multiplatform web applications.
         </p>
         <div>
-          <button>View Work <HiArrowNarrowRight /></button>
+          <button>
+            View Work <HiArrowNarrowRight />
+          </button>
+          
         </div>
       </div>
     </div>
