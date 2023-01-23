a3b33fe8369677b32398236dadc620e4e0d99196
Jackson <z5397791@ad.unsw.edu.au>
2023-01-23 21:23:29 +1100

    Add name, email and message input box in contact component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Skills />
       <Work />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form action="" className="flex flex-col max-w-[600px] w-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            I'm currently open to new opportunities - email@email.com
+          </p>
+        </div>
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="mt-4 p-2 bg-[#ccd6f6]"
+          name="message"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
