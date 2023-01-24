1ac7ca658d50b8e0cbd8bfcc3b28e36ee27483d6
Jackson <z5397791@ad.unsw.edu.au>
2023-01-24 13:36:49 +1100

    Replace electron with typescript in experience component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,25 +7,25 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node-js.png';
 import FireBase from '../assets/firebase.png';
 import Tailwind from '../assets/tailwind.png';
-import Electron from '../assets/electron.png';
+import TypeScript from '../assets/typescript.png';
 import Skill from './Skill';
 
 function Skills() {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-[50vh] bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+          <p className='text-4xl font-bold inline border-b-4 border-[#64ffda]'>Experience</p>
           <p className='py-4'>These are the technologies I've worked with!</p>
         </div>
 
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center pt-8'>
           <Skill image={HTML} title={'HTML'} />
           <Skill image={CSS} title={'CSS'} />
           <Skill image={JavaScript} title={'JavaScript'} />
+          <Skill image={TypeScript} title={'TypeScript'} />
           <Skill image={ReactImg} title={'React'} />
-          <Skill image={Electron} title={'Electron'} />
           <Skill image={FireBase} title={'FireBase'} />
           <Skill image={Tailwind} title={'Tailwind'} />
           <Skill image={Node} title={'Node'} />
