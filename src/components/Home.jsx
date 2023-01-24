bdfe17759a616e6c9477ce1a8ac0924ab4c6a144
Jackson <z5397791@ad.unsw.edu.au>
2023-01-24 16:23:01 +1100

    Add additional text to description in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#64ffda] font-bold tracking-wider md:opacity-80 mb-4">
+        <p className="text-[#64ffda] tracking-wider mb-4">
           Hi, my name is
         </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] mb-3">
@@ -17,7 +17,10 @@ function Home() {
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[500px] mb-4">
           I have a strong passion for software engineering with experience
-          building responsive multiplatform web applications.
+          building responsive multiplatform web applications. Currently, I'm
+          focused on making interactive websites with{" "}
+          <span className="text-[#64ffda]">React</span> and{" "}
+          <span className="text-[#64ffda]">NextJs</span>.
         </p>
         <div>
           <Link to="about" smooth={true} duration={500}>
