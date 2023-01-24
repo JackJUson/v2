20ffcd8360d5d38d8ecd74fde2df292bf7983f36
Jackson <z5397791@ad.unsw.edu.au>
2023-01-24 21:16:16 +1100

    Adjust responsive width size and height for skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,9 +12,9 @@ import Skill from './Skill';
 
 function Skills() {
   return (
-    <div name='skills' className='w-full h-[650px] bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full md:h-[50vh] md:min-h-[600px] bg-[#0a192f] text-gray-300'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      <div className='max-w-[1000px] mx-auto md:p-4 md:pb-4 p-8 pb-[50px] flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-[#64ffda]'>Experience</p>
           <p className='py-4'>These are the technologies I've worked with!</p>
