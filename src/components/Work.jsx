1ab9a928653e3e5e1821d0036f255948fe407d34
Jackson <z5397791@ad.unsw.edu.au>
2023-01-25 23:20:40 +1100

    Add project container for each project object from data

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,80 +1,136 @@
 import React from "react";
-import Project from "../assets/project.jpg";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+import { projects } from "../data/projects";
 
 function Work() {
   return (
-    <div name="work" className="w-full sm:h-screen text-gray-300 bg-[#0a192f]">
+    <div name="work" className="w-full text-[#ccd6f6] bg-[#0a192f] pt-11">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#64ffda]">
-            Projects I've Built
-          </p>
+        <div className="my-11">
+          <h1 className="text-4xl font-bold inline border-b-4 border-[#64ffda]">
+            Work
+          </h1>
         </div>
 
-        {/* Container */}
-        {/* <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-           Item 
-          <div
-            style={{ backgroundImage: `url(${Project})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center
-              items-center mx-auto content-div"
-          >
-             Hover Effects 
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-
-              <div className="pt-8 text-center">
-                <a href="">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2 bg-white 
-                  text-gray-700 text-lg font-bold"
-                  >
-                    Demo
-                  </button>
+        {projects.map((project, index) =>
+          index % 2 === 0 ? (
+            <div className="flex max-h-[50%] mb-[4rem]" key={index}>
+              <div className="rounded-sm overflow-hidden lg:block flex items-center shadow-2xl">
+                <a
+                  href={project.github}
+                  target="_blank"
+                  className="relative group"
+                >
+                  <div
+                    className="bg-[#0f8a6d] bg-opacity-30 absolute rounded-md 
+                          group-hover:bg-opacity-0 z-30 w-full h-full duration-200"
+                  ></div>
+                  <img
+                    src={project.image}
+                    alt="Project Image"
+                    className="w-full relative grayscale-[100%] group-hover:grayscale-0"
+                  />
                 </a>
-                <a href="">
-                  <button
-                    className="text-center rounded-lg px-4 py-3 m-2 bg-white 
-                  text-gray-700 text-lg font-bold"
+              </div>
+              <div className="text-right items-end flex flex-col justify-evenly min-w-[300px]">
+                <div>
+                  <h3 className="text-[#64ffda] text-sm tracking-wide mb-2">
+                    Featured Project
+                  </h3>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="text-3xl font-bold tracking-wide hover:text-[#64ffda] duration-200"
                   >
-                    Code
-                  </button>
+                    {project.title}
+                  </a>
+                </div>
+                <p className="bg-[#112240] w-[110%] p-6 pr-9 z-40 rounded-sm shadow-xl min-w-[400px] text-[#a8b2d1]">
+                  {project.description}
+                </p>
+                <ul className="flex justify-between text-sm w-[90%] z-40 font-thin">
+                  {project.techs.map((tech) => (
+                    <li>{tech}</li>
+                  ))}
+                </ul>
+                <div className="flex">
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="mr-6 hover:text-[#64ffda] duration-200"
+                  >
+                    <FiGithub size={20} />
+                  </a>
+                  <a
+                    href={project.live}
+                    target="_blank"
+                    className="hover:text-[#64ffda] duration-200"
+                  >
+                    <FiExternalLink size={20} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex max-h-[50%] mb-[4rem]" key={index}>
+              <div className="text-left items-start flex flex-col justify-evenly min-w-[400px]">
+                <div>
+                  <h3 className="text-[#64ffda] text-sm tracking-wide mb-2">
+                    Featured Project
+                  </h3>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="text-3xl font-bold tracking-wide hover:text-[#64ffda] duration-200"
+                  >
+                    {project.title}
+                  </a>
+                </div>
+                <p className="bg-[#112240] w-[110%] p-6 pl-9 z-40 rounded-sm shadow-xl text-[#a8b2d1]">
+                  {project.description}
+                </p>
+                <ul className="flex justify-between text-sm w-[90%] z-40 font-thin">
+                  {project.techs.map((tech) => (
+                    <li>{tech}</li>
+                  ))}
+                </ul>
+                <div className="flex">
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="mr-6 hover:text-[#64ffda] duration-200"
+                  >
+                    <FiGithub size={20} />
+                  </a>
+                  <a
+                    href={project.live}
+                    target="_blank"
+                    className="hover:text-[#64ffda] duration-200"
+                  >
+                    <FiExternalLink size={20} />
+                  </a>
+                </div>
+              </div>
+              <div className="rounded-sm overflow-hidden md:block flex items-center shadow-2xl">
+                <a
+                  href={project.github}
+                  target="_blank"
+                  className="relative group"
+                >
+                  <div
+                    className="bg-[#64ffda] bg-opacity-30 absolute rounded-sm 
+                          group-hover:bg-opacity-0 z-30 w-full h-full duration-200"
+                  ></div>
+                  <img
+                    src={project.image}
+                    alt="Project Image"
+                    className="w-full relative grayscale-[100%] group-hover:grayscale-0"
+                  />
                 </a>
               </div>
             </div>
-          </div>
-        </div>  */}
-
-        <div className="flex max-h-[50%]">
-          <div className="rounded-sm overflow-hidden md:block flex items-center w-[240%]">
-            <img src={Project} alt="Project Image" className="w-full"/>
-          </div>
-          <div className="text-right flex flex-col justify-evenly items-end">
-            <h3>Featured Project</h3>
-            <h2>Project Title</h2>
-            <p className="bg-[#112240] w-[110%] py-6 pr-6 pl-6 z-40 rounded-sm">
-              Project Description Lorem, ipsum dolor sit amet consectetur
-              adipisicing elit. culpa debitis similique. Facilis,
-              vitae sapiente? Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Pariatur quas rerum minima commodi
-            </p>
-            <ul className="flex justify-between">
-              <li>React</li>
-              <li>JavaScript</li>
-              <li>TailwindCss</li>
-              <li>Vercel</li>
-              <li>NextJs</li>
-            </ul>
-            <div>
-              <a href="" className="mr-6">
-                Github Icon
-              </a>
-              <a href="">Link Icon</a>
-            </div>
-          </div>
-        </div>
+          )
+        )}
       </div>
     </div>
   );
