9351615cd60bd97545a32f9b5e134e7439b3e948
Jackson <z5397791@ad.unsw.edu.au>
2023-01-25 23:16:04 +1100

    Add social component to home page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Social from "./components/Social";
 
 function App() {
+  AOS.init();
   return (
     <div>
       <Navbar/>
+      <Social />
       <Home />
       <About />
       <Skills />
