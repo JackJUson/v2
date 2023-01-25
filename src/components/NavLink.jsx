1ee64e2c731b640b89a6a5338787d9bd21bd6630
Jackson <z5397791@ad.unsw.edu.au>
2023-01-25 19:38:40 +1100

    Add cursor pointer to list item in nav links

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-scroll";
 
 function NavLink({ screen, link, name, handleClick }) {
   return screen ? (
-    <li className="mx-4 hover:text-[#64ffda] duration-300">
+    <li className="mx-4 hover:text-[#64ffda] duration-300 cursor-pointer">
       <Link to={link} smooth={true} duration={500}>
         {name}
       </Link>
     </li>
   ) : (
-    <li className="my-6 text-4xl hover:text-[#64ffda] duration-300">
+    <li className="my-6 text-4xl hover:text-[#64ffda] duration-300 cursor-pointer">
       <Link onClick={handleClick} to={link} smooth={true} duration={500}>
       {name}
       </Link>
