a4a80913f48ae55d42a8c073b539eb09eee2d01b
Jackson <z5397791@ad.unsw.edu.au>
2023-01-27 13:42:03 +1100

    Adjust header text font size responsiveness to screen size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ function Home() {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#64ffda] tracking-wider mb-4">
+      <div className="max-w-[80%] w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#64ffda] tracking-wider mb-4 font-bold">
           Hi, my name is
         </p>
-        <h1 className="text-5xl sm:text-7xl font-bold text-[#ccd6f6] mb-3">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-[#ccd6f6] mb-3">
           Jackson Jung.
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0] mb-4">
+        <h2 className="text-4xl md:text-5xl lg:text-7xl font-bold text-[#8892b0] mb-4">
           I'm a Frontend Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[500px] mb-4">
