dc4d3a481f1db6f50cf9c5ffc588876a64b067ca
Jackson <z5397791@ad.unsw.edu.au>
2023-01-27 23:18:53 +1100

    Update responsive styling to contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,17 +4,16 @@ function Contact() {
   return (
     <div
       name="contact"
-      className="w-full h-[50vh] min-h-[600px] bg-[#0a192f] flex flex-col justify-center items-center 
-      p-4 text-[#8892b0] text-center"
+      className="w-full h-[50vh] min-h-[600px] bg-[#0a192f] text-[#8892b0] text-center"
     >
-      <div className="h-full flex flex-col justify-center items-center">
+      <div className="h-full max-w-[80%] w-[1000px] mx-auto flex flex-col justify-center items-center">
         <h3 className="text-[#64ffda] tracking-wider font-bold">
           What's Next?
         </h3>
-        <h1 className="text-6xl text-gray-300 block font-bold my-6">
+        <h1 className="text-5xl md:text-6xl text-gray-300 block font-bold my-6">
           Get In Touch
         </h1>
-        <p className="max-w-[600px]">
+        <p className="max-w-[600px] mx-8">
           I am currently looking for new opportunities so my inbox is always
           open. Whether you have a question or just want to say hi, please send
           me a message and I'll get back to you!
