cb87979ab445b0b0405a94dc66e29cfbdd323547
Jackson <z5397791@ad.unsw.edu.au>
2023-01-27 12:34:07 +1100

    Add social links to anchor tags

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -13,7 +13,7 @@ function Social() {
         <div className="h-full flex flex-col justify-center items-center">
           <ul className="h-full flex flex-col justify-between mb-10">
             <li>
-              <a href="">
+              <a href="https://github.com/JackJUson" target="_blank">
                 <FiGithub
                   size={20}
                   className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
@@ -21,7 +21,10 @@ function Social() {
               </a>
             </li>
             <li>
-              <a href="">
+              <a
+                href="https://www.linkedin.com/in/jackson-jung-07837616a/"
+                target="_blank"
+              >
                 <FiLinkedin
                   size={20}
                   className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
@@ -29,7 +32,10 @@ function Social() {
               </a>
             </li>
             <li>
-              <a href="">
+              <a
+                href="https://discordapp.com/users/169382718587535360"
+                target="_blank"
+              >
                 <RxDiscordLogo
                   size={20}
                   className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
@@ -37,7 +43,10 @@ function Social() {
               </a>
             </li>
             <li>
-              <a href="">
+              <a
+                href="https://www.instagram.com/jacksonwjung/?hl=en"
+                target="_blank"
+              >
                 <FaInstagram
                   size={20}
                   className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
@@ -45,7 +54,7 @@ function Social() {
               </a>
             </li>
             <li>
-              <a href="">
+              <a href={Resume} target="_blank">
                 <BsPerson
                   size={20}
                   className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
@@ -59,7 +68,8 @@ function Social() {
       <div className="fixed bottom-[128px] right-[-125px] rotate-90 tracking-[0.2em] w-[400px]">
         <div className="flex items-center">
           <a
-            href=""
+            href="mailto:jackson.w.jung@gmail.com"
+            target="_blank"
             className="mr-11 hover:text-[#64ffda] duration-200 transform hover:-translate-x-1.5 "
           >
             jackson.w.jung@gmail.com
