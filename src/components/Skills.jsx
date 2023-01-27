bf98b24e6fcd12f4aa1b07426b08126e283e4ef5
Jackson <z5397791@ad.unsw.edu.au>
2023-01-27 19:29:39 +1100

    Complete responsive styling for skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,15 +12,15 @@ import Skill from './Skill';
 
 function Skills() {
   return (
-    <div name='skills' className='w-full md:h-[50vh] md:min-h-[600px] bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-full bg-[#0a192f] text-gray-300'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto md:p-4 md:pb-4 p-8 pb-[50px] flex flex-col justify-center w-full h-full'>
+      <div className='lg:h-[50vh] min-h-[700px] max-w-[80%] w-[1000px] mx-auto flex flex-col justify-center h-full px-11 py-[4rem]'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-[#64ffda]'>Experience</p>
           <p className='py-4'>These are my favourite technologies!</p>
         </div>
 
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center pt-8'>
+        <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-center pt-8'>
           <Skill image={HTML} title={'HTML'} />
           <Skill image={CSS} title={'CSS'} />
           <Skill image={JavaScript} title={'JavaScript'} />
