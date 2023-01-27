77374325374a63d998cd986f1bd8a6a2c25f76f4
Jackson <z5397791@ad.unsw.edu.au>
2023-01-27 12:19:51 +1100

    Change social links within social component

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,39 +1,72 @@
 import React from "react";
-import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsPerson } from "react-icons/bs";
+import { RxDiscordLogo } from "react-icons/rx";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa";
 import Resume from "../assets/cv.pdf";
 import SocialLink from "./SocialLink";
 
 function Social() {
   return (
-    <div className="hidden lg:flex fixed flex-col top-[40%] left-0">
-      <ul>
-        <SocialLink
-          name={"Linkedin"}
-          link={"https://www.linkedin.com/in/jackson-jung-07837616a/"}
-          background={"bg-[#0A66C2]"}
-          icon={<FaLinkedin size={30} />}
-        />
-        <SocialLink
-          name={"Github"}
-          link={"https://github.com/JackJUson"}
-          background={"bg-[#333333]"}
-          icon={<FaGithub size={30} />}
-        />
-        <SocialLink
-          name={"Email"}
-          link={"mailto:jackson.w.jung@gmail.com"}
-          background={"bg-[#6fc2b0]"}
-          icon={<HiOutlineMail size={30} />}
-        />
-        <SocialLink
-          name={"Resume"}
-          link={Resume}
-          background={"bg-[#565f69]"}
-          icon={<BsFillPersonLinesFill size={30} />}
-        />
-      </ul>
+    <div className="hidden lg:flex text-[#ccd6f6] text-xs">
+      <div className="fixed bottom-0 left-[55px] flex-col h-[340px]">
+        <div className="h-full flex flex-col justify-center items-center">
+          <ul className="h-full flex flex-col justify-between mb-10">
+            <li>
+              <a href="">
+                <FiGithub
+                  size={20}
+                  className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FiLinkedin
+                  size={20}
+                  className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <RxDiscordLogo
+                  size={20}
+                  className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaInstagram
+                  size={20}
+                  className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <BsPerson
+                  size={20}
+                  className="hover:text-[#64ffda] duration-300 transform hover:-translate-y-1"
+                />
+              </a>
+            </li>
+          </ul>
+          <div className="h-2/5 border-r-[1px] border-[#8892b0]"></div>
+        </div>
+      </div>
+      <div className="fixed bottom-[128px] right-[-125px] rotate-90 tracking-[0.2em] w-[400px]">
+        <div className="flex items-center">
+          <a
+            href=""
+            className="mr-11 hover:text-[#64ffda] duration-200 transform hover:-translate-x-1.5 "
+          >
+            jackson.w.jung@gmail.com
+          </a>
+          <div className="w-full h-full border-t-[1px] border-[#8892b0]"></div>
+        </div>
+      </div>
     </div>
   );
 }
