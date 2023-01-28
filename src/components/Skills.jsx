4ecd0606adcc6bfcb69d9a62f76feacf863047bb
Jackson <z5397791@ad.unsw.edu.au>
2023-01-28 18:16:34 +1100

    Add fade up animation to skill component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
+import React from "react";
 
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node-js.png';
-import FireBase from '../assets/firebase.png';
-import Tailwind from '../assets/tailwind.png';
-import TypeScript from '../assets/typescript.png';
-import Skill from './Skill';
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node-js.png";
+import FireBase from "../assets/firebase.png";
+import Tailwind from "../assets/tailwind.png";
+import TypeScript from "../assets/typescript.png";
+import Skill from "./Skill";
 
 function Skills() {
   return (
-    <div name='skills' className='w-full h-full bg-[#0a192f] text-gray-300'>
+    <div name="skills" className="w-full h-full bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className='lg:h-[50vh] min-h-[700px] max-w-[80%] w-[1000px] mx-auto flex flex-col justify-center h-full px-11 py-[4rem]'>
+      <div
+        className="lg:h-[50vh] min-h-[700px] max-w-[80%] w-[1000px] 
+      mx-auto flex flex-col justify-center h-full px-11 py-[4rem]"
+        data-aos="fade-up"
+        data-aos-easing="ease-out"
+        data-aos-delay="500"
+        data-aos-duration="600"
+      >
         <div>
-          <p className='text-4xl font-bold inline border-b-4 border-[#64ffda]'>Experience</p>
-          <p className='py-4'>These are my favourite technologies!</p>
+          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda]">
+            Experience
+          </p>
+          <p className="py-4">These are my favourite technologies!</p>
         </div>
 
-        <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-center pt-8'>
-          <Skill image={HTML} title={'HTML'} />
-          <Skill image={CSS} title={'CSS'} />
-          <Skill image={JavaScript} title={'JavaScript'} />
-          <Skill image={TypeScript} title={'TypeScript'} />
-          <Skill image={ReactImg} title={'React'} />
-          <Skill image={FireBase} title={'FireBase'} />
-          <Skill image={Tailwind} title={'Tailwind'} />
-          <Skill image={Node} title={'Node'} />
+        <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 text-center pt-8">
+          <Skill image={HTML} title={"HTML"} />
+          <Skill image={CSS} title={"CSS"} />
+          <Skill image={JavaScript} title={"JavaScript"} />
+          <Skill image={TypeScript} title={"TypeScript"} />
+          <Skill image={ReactImg} title={"React"} />
+          <Skill image={FireBase} title={"FireBase"} />
+          <Skill image={Tailwind} title={"Tailwind"} />
+          <Skill image={Node} title={"Node"} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
